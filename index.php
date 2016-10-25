c0a9403f8ef7a4536bdd3dd9a52cbe356f27b423
lgcidb <lgcid@alumnos.ubiobio.cl>
2016-10-25 12:19:00 -0300

    Se completa el codigo index

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-	echo "Hola mundo";
-<?
+<!DOCTYPE>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>
+		<?php echo "Hola mundo"; ?>
+	</h1>
+	<h1>
+		<?php
+			for($i=2; $i<6; $i++){
+				echo "<h$i>¿Cómo estás?</h$i>";
+			} 
+		?>
+	</h1>
+</body>
+</html>
